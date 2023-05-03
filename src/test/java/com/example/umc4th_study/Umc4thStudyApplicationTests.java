f744302e4a68cc6973430e633c729307587d9072
yulee <dbfl0461@gmail.com>
2023-05-03 20:44:55 +0900

    Initial commit

---
+++ src/test/java/com/example/umc4th_study/Umc4thStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc4th_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc4thStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
