9b3a4e7aa38d352867a0701ce4594fa07b36d893
yulee <dbfl0461@gmail.com>
2023-05-08 20:15:50 +0900

    gitignore 적용

---
+++ src/main/java/com/example/umc4th_study/Umc4thStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc4th_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc4thStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc4thStudyApplication.class, args);
+	}
+
+}
